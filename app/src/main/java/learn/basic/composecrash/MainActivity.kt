10a31cc134cc35d276d6546b9c6dcfad0e0e45b4
NeolaxG <heo32.super@gmail.com>
2026-01-03 20:37:42 +0500

    Example name lsit

--- app/src/main/java/learn/basic/composecrash/MainActivity.kt
+++ app/src/main/java/learn/basic/composecrash/MainActivity.kt
@@ -9,15 +9,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,38 +47,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCrashTheme {
-
-                Clicker()
+                NameList()
             }
         }
     }
 }
 
 @Composable
-fun Clicker() {
-    // позволяет запоминать значение сделанное изменяемым для перекомановки элементов
-    var counter by remember {
-        mutableStateOf(0
-        )
-    }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // текст перекомануется (перерисовывается), при изменении текста
-        Text(
-            text = counter.toString(),
-            fontSize = 32.sp
-        )
-        // кнопка не перекомпановывается т.к. не изменяется
-        Button(onClick = {
-            counter++
-        }) {
-            Text(
-                text = "Click Me!",
-                fontSize = 32.sp
+fun NameList() {
+     var name by remember { mutableStateOf("") }
+     var names by remember { mutableStateOf(listOf<String>()) }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    )
+    {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = {text ->
+                    name = text
+                },
+                // вес элемента, если 1 занимает все место которое осталось от других (кнопки)
+                modifier = Modifier.weight(1f)
             )
+
+            // пустой элемент для создания отсупов
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(onClick = {
+                if (name.isNotBlank()){
+                    names = names + name
+                    name = ""
+                }
+            }) {
+                Text(text = "Add")
+            }
+        }
+
+        LazyColumn {
+            items(names){ currentName ->
+                Text(
+                    text = currentName, 
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+                // разделитель элементов
+                Divider()
+            }
         }
     }
 }
@@ -80,6 +107,6 @@ fun Clicker() {
 @Composable
 fun GreetingPreview() {
     ComposeCrashTheme {
-        Clicker()
+        NameList()
     }
 }
